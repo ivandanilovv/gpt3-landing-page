93a85474108a0b7e7d5f6e7e927e7d187c9c9f22
Ivan <ivandanilov282@gmail.com>
2023-02-03 16:16:46 +0100

    Importing the component

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="custom-bg-color">
     <hero-section/>
-    <social-media-links/>
   </div>
 </template>
 
 <script>
 import CustomNavbar from "~/components/CustomNavbar";
 import HeroSection from "@/components/HeroSection";
-import SocialMediaLinks from "@/components/SocialMediaLinks";
 
 export default {
   name: 'IndexPage',
-  components: {SocialMediaLinks, HeroSection, CustomNavbar}
+  components: {HeroSection, CustomNavbar}
 }
 </script>
