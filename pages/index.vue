d320fdfd3b95e7fff34309709daf81068dae28a3
Ivan <ivandanilov282@gmail.com>
2023-02-03 16:15:05 +0100

    Created Social media links component

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="custom-bg-color">
     <hero-section/>
+    <social-media-links/>
   </div>
 </template>
 
 <script>
 import CustomNavbar from "~/components/CustomNavbar";
 import HeroSection from "@/components/HeroSection";
+import SocialMediaLinks from "@/components/SocialMediaLinks";
 
 export default {
   name: 'IndexPage',
-  components: {HeroSection, CustomNavbar}
+  components: {SocialMediaLinks, HeroSection, CustomNavbar}
 }
 </script>
