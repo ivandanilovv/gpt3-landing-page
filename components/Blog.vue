83384297c3e80dc5e236fb8b27f20e747610328e
Ivan <ivandanilov282@gmail.com>
2023-02-14 15:01:55 +0100

    Added cards to the block section

--- components/Blog.vue
+++ components/Blog.vue
@@ -1,15 +1,49 @@
 <template>
   <div class="container py-5">
-    <h1 class="text-gradient m-0 py-2">
+    <h1 class="text-gradient m-0 py-2 text-center text-lg-start">
       A lot is happening,<br/>
       We are blogging about it.
     </h1>
+    <div class="row my-2">
+      <div v-for="cardDetails in blogCards" class="col col-12 col-md-6 col-lg-4">
+        <blog-card :card-details="cardDetails"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import BlogCard from "@/components/BlogCard";
 export default {
-  name: "Blog"
+  name: "Blog",
+  components: {BlogCard},
+  data() {
+    return {
+      blogCards: [
+        {
+          image: require('../assets/images/blog-cards/image2.svg'),
+          alt: 'Image  1',
+          date: 'Feb 14, 2023',
+          title: `GPT-3 and Open  AI is the future. Let us explore how it is?`,
+          link: '#',
+        },
+        {
+          image: require('../assets/images/blog-cards/image3.svg'),
+          alt: 'Image  1',
+          date: 'Feb 14, 2023',
+          title: `GPT-3 and Open  AI is the future. Let us explore how it is?`,
+          link: '#',
+        },
+        {
+          image: require('../assets/images/blog-cards/image4.svg'),
+          alt: 'Image  1',
+          date: 'Feb 14, 2023',
+          title: `GPT-3 and Open  AI is the future. Let us explore how it is?`,
+          link: '#',
+        },
+      ]
+    }
+  }
 }
 </script>
 
---
+++ components/BlogCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bg-secondary p-2 rounded-4 mt-5">
+    <div class="ratio ratio-41x18">
+      <img :src="cardDetails.image" :alt="cardDetails.alt" class="rounded-pill">
+    </div>
+    <div class="bg-secondary text-white p-4">
+      <p>
+        {{ cardDetails.date }}
+      </p>
+      <h5 class="mb-5">
+        {{ cardDetails.title }}
+      </h5>
+      <a :href="cardDetails.link" class="text-decoration-none text-white">
+        Read Full Article
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogCard",
+  props: {
+    cardDetails: Object
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
