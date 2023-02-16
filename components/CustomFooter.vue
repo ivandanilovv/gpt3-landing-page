8236d29f612b20e2de69f2c1cacdd3dbe78ac4be
Ivan <ivandanilov282@gmail.com>
2023-02-16 12:46:56 +0100

    Added footer items

--- components/CustomFooter.vue
+++ components/CustomFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-secondary py-5">
     <div class="container">
-      <div class="d-flex flex-column justify-content-center align-items-center text-center">
+      <div class="d-flex flex-column justify-content-center align-items-center text-center py-5">
         <h1 class="text-gradient mt-5">
           Do you want to step in to the<br/> future before others
         </h1>
@@ -9,13 +9,85 @@
           Request Early Access
         </button>
       </div>
+      <div class="row py-5">
+        <div class="col col-3 d-flex flex-row align-items-start justify-content-center">
+          <img src="../assets/images/custom-footer/logo-footer.svg" alt="Logo"/>
+        </div>
+        <div v-for="(item, i) in footerItems" class="col col-2 d-flex flex-column align-items-start"
+          :class="{'offset-3' : i === 0}">
+          <h5 class="text-white">
+            {{ item.header }}
+          </h5>
+          <ul class="m-0 p-0">
+            <li v-for="link in item.links" class="py-3">
+              <a :href="link.src" class="text-decoration-none text-white">
+                {{ link.text }}
+              </a>
+            </li>
+          </ul>
+          <ul class="m-0 p-0 text-white">
+            <li v-for="paragraph in item.paragraphs" class="py-3">
+              <p class="m-0">
+                {{paragraph}}
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "CustomFooter"
+  name: "CustomFooter",
+  data() {
+    return {
+      footerItems: [
+        {
+          header: 'Links',
+          links: [
+            {
+              src: '#',
+              text: 'Overons',
+            },
+            {
+              src: '#',
+              text: 'Social Media',
+            },
+            {
+              src: '#',
+              text: 'Counters',
+            },
+          ]
+        },
+        {
+          header: 'Company',
+          links: [
+            {
+              src: '#',
+              text: 'Terms & Conditions',
+            },
+            {
+              src: '#',
+              text: 'Privacy Policy',
+            },
+            {
+              src: '#',
+              text: 'Contact',
+            },
+          ]
+        },
+        {
+          header: 'Get in touch',
+          links: [],
+          paragraphs: [
+            'Crechterwoord K12 182 DK Alknjkcb', '085-132567', 'info@payme.net'
+          ]
+        },
+      ]
+    }
+  }
 }
 </script>
 
@@ -23,5 +95,8 @@ export default {
 h1 {
   font-size: 62px;
   font-weight: 800;
+}
+ul {
+  list-style: none;
 }
 </style>
